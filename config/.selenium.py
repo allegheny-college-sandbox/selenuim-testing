4cc0a8cea25811f87597ae16d948cd8c3ffa370f
dluman <dluman@allegheny.edu>
2022-03-12 13:14:05 -0500

    Testing with nodeploy

--- config/.selenium.py
+++ config/.selenium.py
@@ -1,15 +1,18 @@
 import os
+import selenium
 from selenium import webdriver
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("/home/dluman/chromedriver")
 driver.maximize_window()
 
-pages_api = os.getenv("PAGES_URL")
-data = json.loads(pages_api)
-endpoint = data["html_url"]
+#pages_api = os.getenv("PAGES_URL")
+#data = json.loads(pages_api)
+#endpoint = data["html_url"]
 
-driver.get(endpoint + "hole-1.html")
+driver.get("https://allegheny-college-sandbox.github.io/selenuim-testing/" + "hole-1.html")
 
+target = driver.find_element_by_css_selector("#target")
 ball = driver.find_element_by_css_selector("#ball")
 
 print(ball.location)
+print(target.location)
